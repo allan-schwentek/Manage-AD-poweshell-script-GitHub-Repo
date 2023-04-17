efb7af22c65f09cfaf77d922e42437140ef4f60f
Allan Schwentek <109552579+allancbs3@users.noreply.github.com>
2023-04-17 11:47:00 -0300

    Update AD-AutoUserCreation-GitHub-Repo.ps1

--- AD-AutoUserCreation-GitHub-Repo.ps1
+++ AD-AutoUserCreation-GitHub-Repo.ps1
@@ -1,2 +1,15 @@
-﻿Import-Csv -Path C:\tmp\User.csv -Delimiter ";" |
+//Generico
+Import-Csv -Path C:\tmp\User.csv -Delimiter ";" |
 foreach {New-ADUser -name $_.fullname -GivenName $_.firstname -Surname $_.lastname -DisplayName $_.fullname -Path "OU=USER,DC=ALLANCBS3,DC=LOCAL" -CannotChangePassword $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true -Enabled $true -SamAccountName $_.username -UserPrincipalName ($_.username +"@schwentek.local") -EmailAddress $_.email -AccountPassword (ConvertTo-SecureString $_.pwd -AsPlainText -Force
+
+// Adiciona user a um grupo
+Import-Csv -Path C:\temp\user.csv -Delimiter ";" -Encoding UTF8  | 
+foreach { Add-ADGroupMember -Identity "Nome do Grupo AD" -Members $_.username}
+
+//Determina o grupo como primary : precisa localizar a ID do grupo
+Import-Csv -Path C:\temp\user.csv -Delimiter ";" -Encoding UTF8  |
+foreach {Set-ADUser -Identity $_.username -Replace @{PrimaryGroupID=8851}}
+
+//Remove o grupo do usuario
+Import-Csv -Path C:\temp\user.csv -Delimiter ";" -Encoding UTF8  | 
+foreach { Remove-ADGroupMember -Identity "Domain Users" -Members $_.username -Confirm:$false}
